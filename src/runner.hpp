733805596c94f52fecf23e690ea4fad6953c634a
Ted Lyngmo <ted@lyncon.se>
2022-01-23 14:35:00 +0100

    Add mutex around writing the result
    Adding a mutex around writing the result to the output file to avoid
    garbled results when multiple threads writes their result.
    
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- src/runner.hpp
+++ src/runner.hpp
@@ -6,6 +6,7 @@
 #include <execution>
 #include <iostream>
 #include <iterator>
+#include <shared_mutex>
 #include <stdexcept>
 #include <string_view>
 #include <vector>
@@ -17,22 +18,22 @@ std::vector<tsv_record> get_basepairs(std::istream& tsvfile) {
 template<class Container>
 int run([[maybe_unused]] const std::string_view& program, [[maybe_unused]] const std::string_view& tsvname, std::istream& tsvfile,
         const Container& chrs, std::ostream& result) {
-
     auto basepairs = get_basepairs(tsvfile);
+    std::shared_mutex mtx_result;
 
-    std::for_each(std::execution::par_unseq, basepairs.begin(), basepairs.end(), [&chrs, &result](auto& bpline) {
+    std::for_each(std::execution::par_unseq, basepairs.begin(), basepairs.end(), [&chrs, &result, &mtx_result](auto& bpline) {
         // count hits
         std::atomic<std::uint32_t> count = 0;
         auto& bp = bpline[6];
 
 #ifdef USE_MEMMEM
-        std::for_each(std::execution::par_unseq, chrs.begin(), chrs.end(),
-            [&bp, &count](const auto& chromosome) {
-                const char* chr_b = &chromosome[0];
-                const char* chr_e = &chromosome[0] + chromosome.size();
-                for(;chr_b != chr_e && (chr_b = (const char*) memmem(chr_b, chr_e - chr_b, &bp[0], bp.size())) != nullptr; chr_b += bp.size()) {
-                    ++count;
-                }
+        std::for_each(std::execution::par_unseq, chrs.begin(), chrs.end(), [&bp, &count](const auto& chromosome) {
+            const char* chr_b = &chromosome[0];
+            const char* chr_e = &chromosome[0] + chromosome.size();
+            for(; chr_b != chr_e && (chr_b = (const char*)memmem(chr_b, chr_e - chr_b, &bp[0], bp.size())) != nullptr;
+                chr_b += bp.size()) {
+                ++count;
+            }
         });
 #else
         // const std::boyer_moore_searcher searcher(bp.begin(), bp.end());
@@ -47,7 +48,8 @@ int run([[maybe_unused]] const std::string_view& program, [[maybe_unused]] const
 #endif
 
         bpline.emplace_back(std::to_string(count));
-        result << bpline << std::flush;
+        std::lock_guard lock(mtx_result);
+        result << bpline;
     });
 
     return 0;
