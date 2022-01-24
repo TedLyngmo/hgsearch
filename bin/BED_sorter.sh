3c26e8f4a9b6ef7d021eea98252f38fd087778f4
Kai Lawson-McDowall <54321575+kai-lawsonmcdowall@users.noreply.github.com>
2022-01-24 11:04:49 +0000

    Update BED_sorter.sh

--- bin/BED_sorter.sh
+++ bin/BED_sorter.sh
@@ -2,7 +2,7 @@
 
 #sorting by chromosome (1st column of a bed file) 
 sort -t$'\t' -k1 -n yourfile.bed
+
 #sorting by chromosome (1st column of a bed file) 
 #make sure to change "x" to whichever column your frequency count may be in, as number of columns between BED files may differ depending on the user. 
- 
 sort -t$'\t' -kx -nr yourfile.bed 
